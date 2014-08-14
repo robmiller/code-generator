f120b37feaec4a3e46b094e59a58b86f7df7abd8
Chris Morgan <me@chrismorgan.info>
2014-08-15 09:23:33 +1000

    Format and refactor more as I would do it.
    One of the more notable changes which is worth explaining is the removal
    of the exit channel. Basically, the process will live until all the
    tasks exit, so there’s no point in the exit channel—it’s entirely
    acceptable to just let main() finish.
    
    Incidentally, even if the channel were staying, it’d be worth while
    changing it from using bool to using ().

--- src/codes.rs
+++ src/codes.rs
@@ -1,29 +1,22 @@
 use std::rand::{task_rng, Rng};
 
 pub fn generate_code(code_format: &str) -> String {
-	let mut code = String::new();
-	for character in code_format.chars() {
-		let random_char = match character {
-			'B' => random_letter(),
-			'1' => random_number(),
-			other_char => other_char
-		};
-		code.grow(1, random_char);
-	}
+    let mut code = String::with_capacity(code_format.len());
+    for character in code_format.chars() {
+        code.push_char(match character {
+            'B' => choose(['A', 'C', 'E', 'F', 'H', 'K', 'L',
+                           'M', 'P', 'R', 'T', 'W', 'X', 'Y']),
+            '1' => choose(['3', '4', '6', '7', '9']),
+            other_char => other_char,
+        });
+    }
 
-	code
+    code
 }
 
-fn random_letter() -> char {
-	let mut rng = task_rng();
-	let letters = ['A', 'C', 'E', 'F', 'H', 'K', 'L', 'M', 'P', 'R', 'T', 'W', 'X', 'Y'];
-	let i = rng.gen_range(0, letters.len());
-	letters[i]
-}
-
-fn random_number() -> char {
-	let mut rng = task_rng();
-	let numbers = ['3', '4', '6', '7', '9'];
-	let i = rng.gen_range(0, numbers.len());
-	numbers[i]
+fn choose(chars: &[char]) -> char {
+    match task_rng().choose(chars) {
+        Some(&c) => c,
+        None => unreachable!(),
+    }
 }
