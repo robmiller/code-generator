e0ed92968a68f66e54a9cbd0dafec2e0be7aad9c
Rob Miller <rob@bigfish.co.uk>
2014-08-15 14:31:15 +0100

    RustDoc for all our methods

--- src/codes.rs
+++ src/codes.rs
@@ -1,5 +1,25 @@
 use std::rand::{task_rng, Rng};
 
+/// Generates a single code in the specified format.
+///
+/// Given a code format, replaces any instance of the letter "B" with
+/// a randomly selected letter, replaces any instance of the number "1"
+/// with a randomly selected number, and leaves all other characters
+/// intact.
+///
+/// No uniqueness checking is done, so repeated calls to `generate_code`
+/// could produce the same string; it's the responsibility of the
+/// calling code to ensure that codes are unique if this quality is
+/// desired.
+///
+/// # Arguments
+///
+/// * `code_format` - The format to generate codes in, e.g.
+///                   "BB11BB11BB11", "ZZ111111", etc.
+///
+/// # Return value
+///
+/// The generated code.
 pub fn generate_code(code_format: &str) -> String {
     let code = code_format.chars().map( |character|
         match character {
@@ -12,12 +32,22 @@ pub fn generate_code(code_format: &str) -> String {
     code
 }
 
+/// Returns a random letter from the available letters in the codeset.
+///
+/// # Return value
+///
+/// A single `char`
 fn random_letter() -> char {
     let mut rng = task_rng();
     let letters = ['A', 'C', 'E', 'F', 'H', 'K', 'L', 'M', 'P', 'R', 'T', 'W', 'X', 'Y'];
     *rng.choose(letters).expect("Failed to select a random letter")
 }
 
+/// Returns a random letter from the available numbers in the codeset.
+///
+/// # Return value
+///
+/// A single `char`
 fn random_number() -> char {
     let mut rng = task_rng();
     let numbers = ['3', '4', '6', '7', '9'];
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,35 @@ use std::collections::hashmap::HashSet;
 
 mod codes;
 
+/// When passed a code format and number of codes on the command line,
+/// generates that many codes in that particular format.
+///
+/// Codes are guaranteed to be unique within the output.
+///
+/// # Examples
+///
+/// 	./code-generator 5 BB11BB11BB11
+/// 	RC93AL64EW63
+/// 	LA34YK47TE93
+/// 	HR74AP94LT49
+/// 	WX49XH39FR46
+/// 	LA63KC47PW34
+/// 
+/// 	./code-generator 5 XX11BB11
+/// 	XX73LH46
+/// 	XX64HF77
+/// 	XX44FE74
+/// 	XX96YM39
+/// 	XX37FF67
+///
+/// # Exit status
+///
+/// 1 if the command line arguments were invalid; 0 on success.
 fn main() {
     let total_codes;
     let code_format;
+
+    let usage = "Usage: code-generator NUMCODES CODEFORMAT";
 
     match parse_args() {
         (Some(n), Some(c)) => {
@@ -13,7 +39,7 @@ fn main() {
             code_format = c;
         },
         _ => {
-            println!("{}", usage());
+            println!("{}", usage);
             os::set_exit_status(1);
             return;
         }
@@ -42,6 +68,16 @@ fn main() {
     return;
 }
 
+/// Spawned in a task; generates codes infinitely and without regard for
+/// their uniqueness, sending each one to the code checking task.
+///
+/// Stops generating codes at the point that its `Sender` is dropped.
+///
+/// # Arguments
+///
+/// * `code_format` - The code format, ultimately passed to
+///                   `codes::generate_code`.
+/// * `tx` - A `Sender`; each code generated will be sent to this.
 fn code_generator(code_format: String, tx: Sender<String>) {
     loop {
         let code = codes::generate_code(code_format.as_slice());
@@ -52,6 +88,26 @@ fn code_generator(code_format: String, tx: Sender<String>) {
     }
 }
 
+/// Spawned in a task; maintains a list of which codes have already been
+/// generated. When passed a new code by `code_generator`, it checks
+/// whether the code already exists; if it does, it does nothing; if it
+/// doesn't, it passes it through to the `print_handler` to be outputted
+/// to the screen.
+///
+/// Finally, this takes care of the total number of codes; once that
+/// number of codes has been generated and passed to the print handler,
+/// it will send the exit message to the main process, causing the whole
+/// program to exit.
+///
+/// # Arguments
+///
+/// * `total_codes` - That total number of codes to generate.
+/// * `rx` - A `Receiver` down which generated codes are passed; see
+///          `code_generator`.
+/// * `printer_tx` - A `Sender` down which unique codes are passed,
+///                  presumably to be outputted to the screen.
+/// * `exit_tx` - Once all codes have been generated, `true` will be
+///               passed to this `Sender`.
 fn code_exists_handler(total_codes: uint, rx: Receiver<String>, printer_tx: Sender<String>, exit_tx: Sender<bool>) {
     let mut existing_codes: HashSet<String> = HashSet::with_capacity(total_codes);
 
@@ -70,6 +126,12 @@ fn code_exists_handler(total_codes: uint, rx: Receiver<String>, printer_tx: Send
     }
 }
 
+/// Outputs codes to the screen.
+///
+/// # Arguments
+///
+/// * `rx` - A `Receiver` to which codes should be passed; each code
+///          passed will be outputted verbatim.
 fn print_handler(rx: Receiver<String>) {
     loop {
         match rx.recv_opt() {
@@ -83,6 +145,14 @@ fn print_handler(rx: Receiver<String>) {
     }
 }
 
+/// Parses command-line arguments; currently, that means the number of
+/// codes to generate and the format in which to generate them.
+///
+/// # Return value
+///
+/// Returns a tuple of two `Option`s; the first an `Option<uint>` for
+/// the number of codes to generate, and the second an `Option<String>`
+/// for the format to generate codes in.
 fn parse_args() -> (Option<uint>, Option<String>) {
     let args = os::args();
 
@@ -102,6 +172,3 @@ fn parse_args() -> (Option<uint>, Option<String>) {
     (num_codes, code_format)
 }
 
-fn usage() -> String {
-    "Usage: code-generator NUMCODES CODEFORMAT".to_string()
-}
